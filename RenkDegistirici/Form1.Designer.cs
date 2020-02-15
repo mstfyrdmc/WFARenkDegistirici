ffd984859ee9ed4bf58ab35bf4219b75f7c36102
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:34:44 +0300

    RenkDegistir

---
+++ RenkDegistirici/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RenkDegistirici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRed = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbGreen = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbBlue = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbRed
+            // 
+            this.tbRed.Location = new System.Drawing.Point(31, 60);
+            this.tbRed.Maximum = 255;
+            this.tbRed.Name = "tbRed";
+            this.tbRed.Size = new System.Drawing.Size(473, 69);
+            this.tbRed.TabIndex = 0;
+            this.tbRed.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kırmızı";
+            // 
+            // tbGreen
+            // 
+            this.tbGreen.Location = new System.Drawing.Point(31, 168);
+            this.tbGreen.Maximum = 255;
+            this.tbGreen.Name = "tbGreen";
+            this.tbGreen.Size = new System.Drawing.Size(473, 69);
+            this.tbGreen.TabIndex = 0;
+            this.tbGreen.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Yeşil";
+            // 
+            // tbBlue
+            // 
+            this.tbBlue.Location = new System.Drawing.Point(31, 276);
+            this.tbBlue.Maximum = 255;
+            this.tbBlue.Name = "tbBlue";
+            this.tbBlue.Size = new System.Drawing.Size(473, 69);
+            this.tbBlue.TabIndex = 0;
+            this.tbBlue.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 240);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mavi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbBlue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbGreen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tbRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar tbRed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tbGreen;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar tbBlue;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
